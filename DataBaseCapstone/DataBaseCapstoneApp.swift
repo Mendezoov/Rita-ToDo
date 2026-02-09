55ad6e51e8d50a5c0a99530d2fa3f320ebc8ecde
Mendez <conarov@gmail.com>
2026-02-09 03:19:57 +0200

    Initial Commit

---
+++ DataBaseCapstone/DataBaseCapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataBaseCapstoneApp.swift
+//  DataBaseCapstone
+//
+//  Created by Mendez on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct DataBaseCapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
